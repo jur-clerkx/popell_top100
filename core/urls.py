7b03e6bae706dea63b02591e2ae545fb0a24cb7b
Jur Clerkx <jur.clerkx@kabisa.nl>
2022-09-14 11:38:49 +0200

    Add spotify search to vote page

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ app_name = "core"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("stemmen/", views.VoteView.as_view(), name="stemmen"),
+    path("api/search/", views.spotipy_search, name="api-search"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
+import json
+import os
+import spotipy
+from django.http import JsonResponse
+from spotipy.oauth2 import SpotifyClientCredentials
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
@@ -8,3 +14,26 @@ class IndexView(TemplateView):
 
 class VoteView(TemplateView):
     template_name = "vote.html"
+
+
+spotipy_credentials = SpotifyClientCredentials(
+    client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+    client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+)
+
+
+def spotipy_search(request):
+    q = request.GET.get("q")
+    spotify = spotipy.Spotify(client_credentials_manager=spotipy_credentials)
+    search_result = spotify.search(q=q, limit=5, type="track", market="NL")
+    found_tracks = search_result["tracks"]["items"]
+    formatted_results = []
+    for track in found_tracks:
+        formatted_results.append(
+            {
+                "id": track["uri"],
+                "text": track["name"] + " - " + track["artists"][0]["name"],
+                "image": track["album"]["images"][0]["url"],
+            }
+        )
+    return JsonResponse({"results": formatted_results})
