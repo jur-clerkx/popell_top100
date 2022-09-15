fcb0e902303050c15f62b4ecd66967a3c22e5c36
Jur Clerkx <jur.clerkx@kabisa.nl>
2022-09-15 12:03:53 +0200

    Refactor spotify search view to class based view

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,5 @@ app_name = "core"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("stemmen/", views.VoteView.as_view(), name="stemmen"),
-    path("api/search/", views.spotipy_search, name="api-search"),
+    path("api/search/", views.SpotipySearchView.as_view(), name="api-search"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 import os
 import spotipy
 from django.http import JsonResponse
+from django.views import View
 from spotipy.oauth2 import SpotifyClientCredentials
 
 from django.views.generic import TemplateView
@@ -20,21 +21,26 @@ spotipy_credentials = SpotifyClientCredentials(
 )
 
 
-def spotipy_search(request):
-    q = request.GET.get("q")
-    spotify = spotipy.Spotify(client_credentials_manager=spotipy_credentials)
-    search_result = spotify.search(q=q, limit=10, type="track", market="NL")
-    found_tracks = search_result["tracks"]["items"]
-    formatted_results = []
-    for track in found_tracks:
-        formatted_results.append(
-            {
-                "id": track["uri"],
-                "track_name": track["name"],
-                "track_artist": ", ".join(
-                    artist["name"] for artist in track["artists"]
-                ),
-                "image": track["album"]["images"][0]["url"],
-            }
+class SpotipySearchView(View):
+    def get(self, request, *args, **kwargs):
+        q = request.GET.get("q")
+        spotify = spotipy.Spotify(
+            client_credentials_manager=spotipy_credentials
         )
-    return JsonResponse({"results": formatted_results})
+        search_result = spotify.search(
+            q=q, limit=10, type="track", market="NL"
+        )
+        found_tracks = search_result["tracks"]["items"]
+        formatted_results = []
+        for track in found_tracks:
+            formatted_results.append(
+                {
+                    "id": track["uri"],
+                    "track_name": track["name"],
+                    "track_artist": ", ".join(
+                        artist["name"] for artist in track["artists"]
+                    ),
+                    "image": track["album"]["images"][0]["url"],
+                }
+            )
+        return JsonResponse({"results": formatted_results})
