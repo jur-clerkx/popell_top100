c001d0aef4084cfe09d6b8c9847ab4768cd97e35
Jur Clerkx <jur.clerkx@kabisa.nl>
2022-09-15 10:44:09 +0200

    Improve selection block appearance

--- core/views.py
+++ core/views.py
@@ -1,10 +1,8 @@
-import json
 import os
 import spotipy
 from django.http import JsonResponse
 from spotipy.oauth2 import SpotifyClientCredentials
 
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
@@ -25,14 +23,15 @@ spotipy_credentials = SpotifyClientCredentials(
 def spotipy_search(request):
     q = request.GET.get("q")
     spotify = spotipy.Spotify(client_credentials_manager=spotipy_credentials)
-    search_result = spotify.search(q=q, limit=5, type="track", market="NL")
+    search_result = spotify.search(q=q, limit=10, type="track", market="NL")
     found_tracks = search_result["tracks"]["items"]
     formatted_results = []
     for track in found_tracks:
         formatted_results.append(
             {
                 "id": track["uri"],
-                "text": track["name"] + " - " + track["artists"][0]["name"],
+                "track_name": track["name"],
+                "track_artist": track["artists"][0]["name"],
                 "image": track["album"]["images"][0]["url"],
             }
         )
