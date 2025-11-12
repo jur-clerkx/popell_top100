31c1b105bcdfb667fcb7015e9e46c88c0a43abde
Jur Clerkx <jur.c@live.nl>
2025-11-12 19:06:26 +0000

    Refactor views

--- core/rest_api/views.py
+++ core/rest_api/views.py
@@ -1,8 +1,17 @@
 from rest_framework import generics
+from dataclasses import asdict
 
-from core.models.voting import HitList
+from core.models.music import Track
+from core.models.voting import HitList, Vote, VoteSubmission
 from core.rest_api.serializers import HitListSerializer
 from core.services.settings import SettingsService
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from core.services.voting import HitListService
+from core.spotify import spotify
+from rest_framework import status
+from rest_framework import permissions
 
 
 class CurrentHitListView(generics.RetrieveAPIView):
@@ -13,3 +22,70 @@ class CurrentHitListView(generics.RetrieveAPIView):
 
     def get_queryset(self):
         return HitList.objects.all()
+
+
+class SpotipySearchView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, format=None):
+        q = request.GET.get("q")
+        search_results = spotify.search_custom_tracks(
+            q
+        ) + spotify.search_tracks(q)
+        return Response({"results": [asdict(x) for x in search_results]})
+
+
+class SpotipyGetView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, spotifyuri, format=None):
+        return Response(asdict(spotify.get_track_by_uri(spotifyuri)))
+
+
+class TrackStatsGetView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, format=None):
+        title = request.GET.get("title", "")
+        year = request.GET.get("year", "")
+        artist = request.GET.get("artist", "")
+        track_uri = request.GET.get("spotify_uri", "")
+        print(title, year, artist, track_uri)
+        hitlist = HitListService.get_by_year(year)
+        track = Track.find_by_uri_or_title(track_uri, title)
+        if track is None:
+            return Response(
+                status=status.HTTP_404_NOT_FOUND,
+                data={"error": "Track not found!"},
+            )
+        votes = Vote.objects.filter(
+            track=track,
+            submission__hit_list=hitlist,
+            submission__is_invalidated=False,
+        )
+        if votes.count() == 0:
+            return Response(
+                status=status.HTTP_404_NOT_FOUND,
+                data={"error": "Track not part of hitlist!"},
+            )
+        position = 1
+        for entry in hitlist.get_list():
+            if entry.id == track.id:
+                break
+            position += 1
+        vote_count, points = 0, 0
+        for vote in votes:
+            vote_count += 1
+            points += vote.points
+        votes = VoteSubmission.objects.filter(
+            hit_list=hitlist, vote__track=track, is_invalidated=False
+        ).values("submitter_name")
+        return Response(
+            {
+                "track": str(track),
+                "position": position,
+                "points": points,
+                "number_of_votes": vote_count,
+                "voters": list(votes),
+            }
+        )
---
+++ core/views/dashboard.py
@@ -0,0 +1,186 @@
+import logging
+from django.shortcuts import get_object_or_404, redirect
+from django.views import View
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    CreateView,
+    UpdateView,
+    FormView,
+    DeleteView,
+)
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from core.forms import MergeTracksForm
+from core.models.music import Track
+from core.models.voting import HitList, VoteSubmission
+from core.services.music import SimilarTrackService
+from core.services.settings import SettingsService
+from core.services.voting import HitListService
+from core.spotify import spotify
+from core.widgets import CustomDateTimeInput
+from tinymce.widgets import TinyMCE  # type: ignore
+
+HITLIST_LIST_URL = "core:hitlist-list"
+DASHBOARD_URL = "core:dashboard"
+
+
+logger = logging.getLogger(__name__)
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = "dashboard/index.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["hitlists"] = HitList.objects.all()
+        ctx["hitlist"] = SettingsService.get_current_hitlist()
+        ctx["vote_count"] = VoteSubmission.objects.filter(
+            hit_list=ctx["hitlist"], is_invalidated=False
+        ).count()
+        ctx["invalidated"] = VoteSubmission.objects.filter(
+            hit_list=ctx["hitlist"], is_invalidated=True
+        ).count()
+        return ctx
+
+
+class HistListListView(LoginRequiredMixin, ListView):
+    model = HitList
+    template_name = "dashboard/cms/hitlistlist.html"
+
+
+class SimilarTrackView(LoginRequiredMixin, TemplateView):
+    template_name = "dashboard/cms/similartracks.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["similar_tracks"] = SimilarTrackService.get_similar_tracks()
+        return ctx
+
+
+class HitListCreateView(LoginRequiredMixin, CreateView):
+    model = HitList
+    template_name = "dashboard/cms/hitlistcreate.html"
+    fields = [
+        "name",
+        "vote_start_date",
+        "vote_end_date",
+        "description",
+        "theme",
+    ]
+    success_url = reverse_lazy(HITLIST_LIST_URL)
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields["vote_start_date"].widget = CustomDateTimeInput()
+        form.fields["vote_end_date"].widget = CustomDateTimeInput()
+        form.fields["description"].widget = TinyMCE()
+        return form
+
+
+class HitListUpdateView(LoginRequiredMixin, UpdateView):
+    model = HitList
+    template_name = "dashboard/cms/hitlistupdate.html"
+    fields = [
+        "name",
+        "vote_start_date",
+        "vote_end_date",
+        "description",
+        "theme",
+        "is_closed",
+    ]
+    success_url = reverse_lazy(HITLIST_LIST_URL)
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields["vote_start_date"].widget = CustomDateTimeInput()
+        form.fields["vote_end_date"].widget = CustomDateTimeInput()
+        form.fields["description"].widget = TinyMCE()
+        return form
+
+
+class HitListDeleteView(LoginRequiredMixin, DeleteView):
+    model = HitList
+    success_url = reverse_lazy(HITLIST_LIST_URL)
+    template_name = "dashboard/cms/hitlistdelete.html"
+
+
+class HitListExportView(LoginRequiredMixin, View):
+    def get(self, request, pk, *args, **kwargs):
+        hitlist = get_object_or_404(HitList, id=pk)
+        return HitListService.export_to_excel(hitlist)
+
+
+class CustomTrackListView(LoginRequiredMixin, ListView):
+    model = Track
+    template_name = "dashboard/cms/tracklist.html"
+
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .prefetch_related("artists")
+            .filter(is_non_spotify=True)
+        )
+
+
+class CustomTrackUpdateView(LoginRequiredMixin, UpdateView):
+    model = Track
+    template_name = "dashboard/cms/trackupdate.html"
+    fields = ["title", "artists", "preview_url", "image_url"]
+    success_url = reverse_lazy("core:custom-track-list")
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields["artists"].queryset = form.fields[
+            "artists"
+        ].queryset.filter(is_non_spotify=True)
+        return form
+
+
+class MergeTracksView(LoginRequiredMixin, FormView):
+    form_class = MergeTracksForm
+    template_name = "dashboard/cms/mergetracks.html"
+    success_url = reverse_lazy(HITLIST_LIST_URL)
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class ToggleSubmissionInvalidation(LoginRequiredMixin, View):
+    def get(self, request, votesubmissionid, *args, **kwargs):
+        submission = VoteSubmission.objects.get(id=votesubmissionid)
+        submission.is_invalidated = not submission.is_invalidated
+        submission.save()
+        return redirect(reverse_lazy(DASHBOARD_URL))
+
+
+class HitListCreateSpotifyPlaylistView(View):
+    def get(self, request, hitlist_id, *args, **kwargs):
+        hitlist = get_object_or_404(HitList, id=hitlist_id)
+        try:
+            HitListService.create_spotify_list(
+                hitlist, request.session.get("spotify_token")
+            )
+        except Exception as e:
+            logger.error("Failed to create spotify playlist!")
+            logger.error(e)
+            request.session["spotify_token"] = None
+        return redirect(reverse_lazy(DASHBOARD_URL))
+
+
+class SpotifyOAuthView(View):
+    def get(self, request):
+        spotify_oauth = spotify.get_spotify_oauth()
+        callback_code = request.GET.get("code", None)
+        if callback_code:
+            # Found callback code, add access token to session
+            access_token = spotify_oauth.get_access_token(callback_code)[
+                "access_token"
+            ]
+            request.session["spotify_token"] = access_token
+            return redirect(reverse_lazy(DASHBOARD_URL))
+        else:
+            return redirect(spotify_oauth.get_authorize_url())
